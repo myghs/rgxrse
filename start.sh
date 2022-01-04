6ee100acd576eddc01fb3411b8802aeb937d38b5
myghs <71456100+myghs@users.noreply.github.com>
2022-01-04 23:32:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=65093e23-d69f-44b7-b460-cfe33c398682
+AUUID=24b4b1e1-7a89-45f6-858c-242cf53b5bdb
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
